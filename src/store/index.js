a2836093d6a87f8d4bc9e5a84cefbe9e9913c262
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-02-10 14:40:46 +0400

    Store user or admin data after registration/login

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from "@/axios";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: null,
+    admin: null
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    setAdmin(state, admin) {
+      state.admin = admin;
+    }
   },
   actions: {
+    async fetchUser({commit}) {
+      const {data} = await axios.get('api/users/me');
+      commit('setUser', data);
+    },
+    async fetchAdmin({commit}) {
+      const {data} = await axios.get('api/admins/me');
+      commit('setAdmin', data);
+    },
+
   },
-  modules: {
-  }
+  modules: {}
 })
