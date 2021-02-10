eb28ffe2f68be521b24d3122da200489d0b31d59
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-02-10 14:45:53 +0400

    Define routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import UserDashboard from "@/views/UserDashboard";
+import Register from "@/views/Register";
+import AddPackage from "@/views/Admin/AddPackage";
+import ChangePackageStatus from "@/views/Admin/ChangePackageStatus";
+import Login from "@/views/Login";
+import AdminLogin from "@/views/Admin/AdminLogin";
+import AdminDashboard from "@/views/Admin/AdminDashboard";
 
 Vue.use(VueRouter)
 
@@ -9,7 +16,45 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+
+  {
+    path: "/register",
+    component: Register
+  },
+
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/adminLogin',
+    component: AdminLogin
+  },
+  {
+    path: '/profile',
+    component: UserDashboard
+  },
+
+  {
+    path: '/admin/changeStatus',
+    component: ChangePackageStatus,
+  },
+
+  {
+    path: '/admin/addPackage',
+    component: AddPackage
+  },
+
+  {
+    path: '/adminDashboard',
+    component: AdminDashboard
+  },
+
+  {
+    path: '/users/:user/packages/:id',
+    component: ChangePackageStatus
+  },
 ]
 
 const router = new VueRouter({
